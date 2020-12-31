9c670dd2d5b54609d1383199ccc07e88f7a4b56e
Nikul Patel <nikulpatel1643@gmail.com>
2020-12-31 12:46:43 +0530

    Update resume and Avatar Size

--- src/views/dark-template/sidebar/Avatar.vue
+++ src/views/dark-template/sidebar/Avatar.vue
@@ -2,11 +2,13 @@
   <div class="text-md-center">
     <v-avatar
       color="grey lighten-4"
-      size="120"
+      size="140"
     >
       <img
         :src="publicPath('/img/avtar.jpg')"
         alt="Nikul Patel"
+        width="100%"
+        height="100%"
       >
     </v-avatar>
   </div>
